70fe358909231bf078ed8df39a5a851cad837e86
Ibrahima Bah <bibrahim97@outlook.com>
2022-09-05 23:32:46 +0100

    log loops log

--- freecodecamp/loops.js
+++ freecodecamp/loops.js
@@ -38,7 +38,6 @@ console.log(product);
 // Setup
 const myDoWhile = [];
 let i = 10;
-
 // Only change code below this line
 do {
   myDoWhile.push(i);
@@ -51,10 +50,10 @@ console.log(i, myDoWhile);
 function sum(arr, n) {
     // Only change code below this line
   if (n <= 0){
-    return 0;
-  } else {
-    return sum(arr, n - 1) + arr[n - 1];
-  }
+        return 0;
+    } else {
+        return sum(arr, n - 1) + arr[n - 1];
+    }
     // Only change code above this line
   }
   
