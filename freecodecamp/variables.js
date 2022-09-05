1c0b8f507312bec022a2e634412d3bf3b3331e5c
Ibrahima Bah <bibrahim97@outlook.com>
2022-09-05 23:26:27 +0100

    log variables update

--- freecodecamp/variables.js
+++ freecodecamp/variables.js
@@ -1,17 +1,17 @@
-// Declare JavaScript Variables
+/* Declare JavaScript Variables */
 var myAge;
 
-// Storing Values with the Assignment Operator
+/* Storing Values with the Assignment Operator */
 myAge = 22;
 
-// Assigning the Value of One Variable to Another
+/* Assigning the Value of One Variable to Another */
 var herAge;
 herAge = myAge;
 
-// Initializing Variables with the Assignment Operator
+/* Initializing Variables with the Assignment Operator */
 var myBirthDay = 150999;
 
-// Declare String Variables
+/* Declare String Variables */
 var myFirstName = "Ibrahima";
 var myLastName = "Bah";
 
