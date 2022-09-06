dcfdfb9be72cbb1a57276d15115de63c3de311c1
Ibrahima Bah <bibrahim97@outlook.com>
2022-09-06 16:56:59 +0100

    log random numbers

--- freecodecamp/basics.js
+++ freecodecamp/basics.js
@@ -4,3 +4,39 @@
 /*
 multiple comments
 */
+
+/* Generate Random Fractions with JavaScript */
+function randomFraction() {
+
+    // Only change code below this line
+  
+    return Math.random();
+  
+    // Only change code above this line
+  }
+  
+  /* Generate Random Whole Numbers with JavaScript */
+  let random = randomFraction();
+  console.log(random);
+
+  function randomWholeNum() {
+
+    // Only change code below this line
+  
+    return Math.floor(Math.random() * 10);
+  }
+  
+  let whole = randomWholeNum();
+  // log result
+  console.log(whole);
+
+  /* Generate Random Whole Numbers within a Range */
+  function randomRange(myMin, myMax) {
+    // Only change code below this line
+    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+    // Only change code above this line
+  }
+  let range = randomRange();
+  
+  // log result
+  console.log(range);
