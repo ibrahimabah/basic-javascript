1f196dc34207fb765c342c0f6ab6267c2d59ed79
Ibrahima Bah <bibrahim97@outlook.com>
2022-09-09 02:27:37 +0100

    log kata training

---
+++ codewars/js-kata-training.js
@@ -0,0 +1,54 @@
+/* Katas list of Training JS series
+    Github Link: https://github.com/myjinxin2015/Katas-list-of-Training-JS-series
+*/
+
+// Training JS #1: create your first JS function and print "Hello World!"
+function helloWorld(){
+    var hello = "Hello World!";
+    console.log(hello);
+}
+helloWorld();
+
+// Training JS #2: Basic data types--Number
+var v1=50;v2=100,v3=150,v4=200,v5=2,v6=250
+function equal1(){
+  var a=v1   
+  var b=v1   
+    let check1 = a+b;
+    console.log(check1);
+}
+equal1();
+
+function equal2(){
+  var a= v4;  //set number value to a
+  var b= v2; //set number value to b
+    let check2 = a-b;
+    console.log(check2);
+}
+equal2();
+
+function equal3(){
+  var a= v1;  //set number value to a
+  var b= v5;  //set number value to b
+    let check3 = a*b;
+    console.log(check3);
+}
+equal3();
+
+function equal4(){
+  var a= v4;  //set number value to a
+  var b= v5;  //set number value to b
+    let check4 = a/b;
+    console.log(check4);
+}
+equal4();
+
+function equal5(){
+  var a=  v6; //set number value to a
+  var b=  v3; //set number value to b
+    let check5 = a%b;
+    console.log(check5);
+}
+equal5();
+
+// Training JS #3: Basic data types--String
