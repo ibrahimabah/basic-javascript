fa1a91adefd61d9499e21c0a4de882fb953bce27
Ibrahima Bah <bibrahim97@outlook.com>
2022-09-14 17:08:18 +0100

    log steps 3 to 5

--- freecodecamp/es6.js
+++ freecodecamp/es6.js
@@ -9,9 +9,9 @@ function checkScope() {
     return i;
   }
   
-  // 02 : Mutate an Array Declared with const
+// 02 : Mutate an Array Declared with const
   const s = [5, 7, 2];
-  function editInPlace() {
+function editInPlace() {
     // Only change code below this line
       s[0] = 2;
       s[1] = 5;
@@ -20,5 +20,32 @@ function checkScope() {
     console.log(s);
   }
   editInPlace();
+
+// 03 : Prevent Object Mutation
+  function freezeObj() {
+    const MATH_CONSTANTS = {
+      PI: 3.14
+    };
+    // Only change code below this line
+      Object.freeze(MATH_CONSTANTS);
+    
+    // Only change code above this line
+    try {
+      MATH_CONSTANTS.PI = 99;
+    } catch(ex) {
+      console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+  }
+  const PI = freezeObj();
+
+// 04 : Use Arrow Functions to Write Concise Anonymous Functions
+    // simplified version with return keyword
+    const magic = () => new Date();
+
+// 05 : Write Arrow Functions with Parameters
+    // simplified version with parameters
+    const myConcat = (arr1, arr2) =>
+    arr1.concat(arr2);
 
   
