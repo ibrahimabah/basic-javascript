0925795aaf14f5e97c8f322260e38eca47724aef
Ibrahima Bah <bibrahim97@outlook.com>
2022-09-19 23:29:53 +0100

    log step 9 & 10

--- freecodecamp/es6.js
+++ freecodecamp/es6.js
@@ -68,6 +68,33 @@ function editInPlace() {
 
 
 // 09 : Use Destructuring Assignment to Extract Values from Objects
+  const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
 
+  /* change : 
+    const today = HIGH_TEMPERATURES.today;
+    const tomorrow = HIGH_TEMPERATURES.tomorrow;
+  */
+  const {today, tomorrow } = HIGH_TEMPERATURES; // result
+
+
+// 10 : Use Destructuring Assignment to Assign Variables from Objects
+  const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
+  /* change ;
+    const highToday = HIGH_TEMPERATURES.today;
+    const highTomorrow = HIGH_TEMPERATURES.tomorrow; 
+  */
+
+    const { today: highToday, tomorrow: highTomorrow} = HIGH_TEMPERATURES; // result
+
+  
+// 11 : Use Destructuring Assignment to Assign Variables from Nested Objects
 
   
