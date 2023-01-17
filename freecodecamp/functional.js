3e0d401f18edd5b80eefb076b84567c740ccb866
Ibrahima Bah <bibrahim97@outlook.com>
2023-01-17 04:03:04 +0300

    created functional.js file and log comment lines 1-9

---
+++ freecodecamp/functional.js
@@ -0,0 +1,9 @@
+// 000 : 
+// -----log steps 1 through 12-----
+// 000 :
+
+// 013 : Combine Two Arrays Using the concat Method
+// 014 : Add Elements to the End of an Array Using concat Instead of push
+/*-----------------------------------LAST-STOP-----------------------------------
+--------------------------------DATE-:-17/01/2023--------------------------------
+*/
