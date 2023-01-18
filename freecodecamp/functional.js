c5a183443cdeb1d309fa77b1a2b23e44ab2696d5
Ibrahima Bah <bibrahim97@outlook.com>
2023-01-18 23:18:21 +0300

    steps 15-18

--- freecodecamp/functional.js
+++ freecodecamp/functional.js
@@ -4,6 +4,9 @@
 
 // 013 : Combine Two Arrays Using the concat Method
 // 014 : Add Elements to the End of an Array Using concat Instead of push
-/*-----------------------------------LAST-STOP-----------------------------------
---------------------------------DATE-:-17/01/2023--------------------------------
-*/
+// 015 : Use the reduce Method to Analyze Data
+// 016 : Use Higher-Order Functions map, filter, or reduce to Solve a Complex Problem
+// 017 : Sort an Array Alphabetically using the sort Method
+//-----------------------------------LAST-STOP-----------------------------------
+// 018 : Return a Sorted Array Without Changing the Original Array
+//------------------------------DATE-:-17/01/2023--------------------------------
