6b066f0aa6468efad45d434e7e65e9cc31d13f05
Ibrahima Bah <bibrahim97@outlook.com>
2023-01-25 00:10:27 +0300

    ...

--- freecodecamp/intalgscrtp.js
+++ freecodecamp/intalgscrtp.js
@@ -1,4 +1,4 @@
-//  01   :   Sum All Numbers in a Range
+//  01   :   -----------------------Sum All Numbers in a Range-----------------------
 function sumAll(arr) {
     let total = 0
     arr.sort((a, b) => a - b);
@@ -10,4 +10,20 @@ function sumAll(arr) {
 
 console.log(sumAll([4, 1]));
 
-//  02  :
+//  02   :    ----------------------------Diff Two Arrays----------------------------
+function diffArray(arr1, arr2) {
+    const newArr = [];
+  
+    function diffArr(one, two) {
+      for(let i = 0; i < one.length; i += 1) {
+        if(two.indexOf(one[i]) === -1) {
+          newArr.push(one[i]);
+        }
+      }
+    }
+    diffArr(arr1, arr2);
+    diffArr(arr2, arr1);
+    return newArr;
+  }
+  
+  console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
